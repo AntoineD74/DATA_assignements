e1ece313608da1e6c6d15341dfa1449c9c28f93c
antoine <antoine.dupuis74@icloud.com>
2023-08-25 08:34:44 +0000

    Second part finished

--- 00_introduction/assignment.py
+++ 00_introduction/assignment.py
@@ -41,14 +41,52 @@ def _plot_three_normals():
     plt.title('Three Normal Distribution Curves')
     plt.show()
 
-    
-"""
+# Part 2.1
 def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
-    # Part 2.1
-
+    sum = 0
+    for i in range(len(weights)):
+        elem1 = weights[i] / (np.sqrt(2 * np.pi * np.power(sigmas[i], 2)))
+        toExp = -(np.power((x - mus[i]), 2)) / (2 * np.power(sigmas[i], 2))
+        elem2 = np.exp(toExp)
+        sum += elem1*elem2
+    return sum
+    
+    
+# Part 2.2
 def _compare_components_and_mixture():
-    # Part 2.2
+    
+    def plot_normal2(sigma: float, mu: float, x_range):   
+        elem1 = 1 / (np.power((2 * np.pi * np.power(sigma, 2)), 0.5))
+        beforeExp = -np.power((x_range - mu), 2) / (2 * np.power(sigma, 2))
+        elem2 = np.exp(beforeExp)
+            
+        plt.plot(x_range, elem1 * elem2, label=f'Sigma {sigma} Mu {mu}')
+    
+    
+    def plot_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
+        sum = 0
+        for i in range(len(weights)):
+            elem1 = weights[i] / (np.sqrt(2 * np.pi * np.power(sigmas[i], 2)))
+            toExp = -(np.power((x - mus[i]), 2)) / (2 * np.power(sigmas[i], 2))
+            elem2 = np.exp(toExp)
+            sum += elem1*elem2
+        plt.plot(x_range, sum, label='Mixture')
+       
+    
+    plt.clf()  # Clear the current figure if there is one
+    
+    x_range = np.linspace(-5, 5, 500)
+    plot_normal2(0.5, 0, x_range)
+    plot_normal2(1.5, -0.5, x_range)
+    plot_normal2(0.25, 1.5, x_range)
+    plot_mixture(x_range, [0.5, 1.5, 0.25], [0, -0.5, 1.5], [1/3, 1/3, 1/3])
 
+    plt.legend()
+    plt.title('Three Normal Distribution Curves + mixture')
+    plt.show()
+    
+   
+"""
 def sample_gaussian_mixture(sigmas: list, mus: list, weights: list, n_samples: int = 500):
     # Part 3.1
 
@@ -57,7 +95,7 @@ def _plot_mixture_and_samples():
 """
 
 if __name__ == '__main__':
-    
+    """
     #Test 1.1
     print(normal(0, 1, 0))
     print(normal(3, 1, 5))
@@ -66,3 +104,10 @@ if __name__ == '__main__':
     #Tests 1.2
     plot_normal(0.5, 0, -2, 2)
     _plot_three_normals()
+    
+    #Tests 2.1
+    print(normal_mixture(np.linspace(-5, 5, 5), [0.5, 0.25, 1], [0, 1, 1.5], [1/3, 1/3, 1/3]))
+    print(normal_mixture(np.linspace(-2, 2, 4), [0.5], [0], [1]))
+    """
+    #Test 2.2
+    _compare_components_and_mixture()
