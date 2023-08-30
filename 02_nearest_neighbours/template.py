89e1d7b7798951978d2d52efe4a1f8b8e3f8c798
antoine <antoine.dupuis74@icloud.com>
2023-08-30 18:15:05 +0000

    Part 1 done

--- 02_nearest_neighbours/template.py
+++ 02_nearest_neighbours/template.py
@@ -14,8 +14,12 @@ def euclidian_distance(x: np.ndarray, y: np.ndarray) -> float:
     '''
     Calculate the euclidian distance between points x and y
     '''
-    ...
-
+    sum = 0
+    for i in range(len(x)):
+        sum += np.power((x[i] - y[i]), 2)
+    
+    sum = np.power(sum, 0.5)
+    return sum
 
 def euclidian_distances(x: np.ndarray, points: np.ndarray) -> np.ndarray:
     '''
@@ -24,8 +28,9 @@ def euclidian_distances(x: np.ndarray, points: np.ndarray) -> np.ndarray:
     '''
     distances = np.zeros(points.shape[0])
     for i in range(points.shape[0]):
-        ...
-    ...
+        distances[i] = euclidian_distance(x, points[i])
+        
+    return distances
 
 
 def k_nearest(x: np.ndarray, points: np.ndarray, k: int):
@@ -33,7 +38,12 @@ def k_nearest(x: np.ndarray, points: np.ndarray, k: int):
     Given a feature vector, find the indexes that correspond
     to the k-nearest feature vectors in points
     '''
-    ...
+    distances = np.zeros(points.shape[0])
+    for i in range(points.shape[0]):
+        distances[i] = euclidian_distance(x, points[i])
+        
+    distances = np.argsort(distances)
+    return distances[:k]
 
 
 def vote(targets, classes):
@@ -41,22 +51,24 @@ def vote(targets, classes):
     Given a list of nearest targets, vote for the most
     popular
     '''
-    ...
+    classes_counts = np.bincount(targets)
+    most_common = np.argmax(classes_counts)
+    return most_common
+    
 
 
-def knn(
-    x: np.ndarray,
-    points: np.ndarray,
-    point_targets: np.ndarray,
-    classes: list,
-    k: int
-) -> np.ndarray:
+def knn(x: np.ndarray, points: np.ndarray,point_targets: np.ndarray, classes: list, k: int) -> np.ndarray:
     '''
     Combine k_nearest and vote
     '''
-    ...
-
-
+    k_closest = k_nearest(x, points, k)
+    k_closest_array = np.zeros(k_closest.shape[0], dtype=int)
+    for i in range(len(k_closest)):
+        k_closest_array[i] = point_targets[k_closest[i]]
+        
+    return vote(k_closest_array, np.array(classes))
+    
+        
 def knn_predict(
     points: np.ndarray,
     point_targets: np.ndarray,
@@ -145,3 +157,34 @@ def compare_knns(
 ):
     # Remove if you don't go for independent section
     ...
+    
+if __name__ == '__main__':
+    d, t, classes = load_iris()
+    plot_points(d, t)
+    
+    d, t, classes = load_iris()
+    x, points = d[0,:], d[1:, :]
+    x_target, point_targets = t[0], t[1:]
+    
+    print("\n[+]Part 1.1")
+    print(euclidian_distance(x, points[0]))
+    print(euclidian_distance(x, points[50]))
+    
+    print("\n[+]Part 1.2")
+    print(euclidian_distances(x, points))
+    
+    print("\n[+]Part 1.3")
+    print(k_nearest(x, points, 1))
+    print(k_nearest(x, points, 3))
+    
+    print("\n[+]Part 1.4")
+    print(vote(np.array([0,0,1,2]), np.array([0,1,2])))
+    print(vote(np.array([1,1,1,1]), np.array([0,1])))
+    print(vote(np.array([0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1, 2, 1, 2, 2, 2 ,1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ,2 ,2 ,2 ,2]), np.array([0,1,2])))
+
+    print("\n[+]Part 1.5")
+    print(knn(x, points, point_targets, classes, 1))
+    print(knn(x, points, point_targets, classes, 5))
+    print(knn(x, points, point_targets, classes, 150))
+
+    
