1d383f492432c0883d54487ce707b149919e3b2c
antoine <antoine.dupuis74@icloud.com>
2023-09-21 11:31:58 +0000

    Add part 1.1 to 1.4

--- 05_classification/template.py
+++ 05_classification/template.py
@@ -1,10 +1,3 @@
-# Author: 
-# Date:
-# Project: 
-# Acknowledgements: 
-#
-
-
 from tools import load_iris, split_train_test
 
 import matplotlib.pyplot as plt
@@ -12,49 +5,49 @@ import numpy as np
 from scipy.stats import multivariate_normal
 
 
-def mean_of_class(
-    features: np.ndarray,
-    targets: np.ndarray,
-    selected_class: int
-) -> np.ndarray:
+def mean_of_class(features: np.ndarray, targets: np.ndarray, selected_class: int) -> np.ndarray:
     '''
     Estimate the mean of a selected class given all features
     and targets in a dataset
     '''
-    ...
+    feature_for_specific_class = []
+    for i in range(len(features)):
+        if(targets[i]==selected_class):
+             feature_for_specific_class.append(features[i])
+             
+    means = []
+    feature_for_specific_class = np.array(feature_for_specific_class)
+    for j in range(features.shape[1]):
+        means.append(np.mean(feature_for_specific_class[:, j]))
+        
+    return np.array(means)
 
 
-def covar_of_class(
-    features: np.ndarray,
-    targets: np.ndarray,
-    selected_class: int
-) -> np.ndarray:
+def covar_of_class(features: np.ndarray, targets: np.ndarray, selected_class: int) -> np.ndarray:
     '''
     Estimate the covariance of a selected class given all
     features and targets in a dataset
     '''
-    ...
+    feature_for_specific_class = []
+    for i in range(len(features)):
+        if(targets[i]==selected_class):
+             feature_for_specific_class.append(features[i])
+             
+    return np.cov(feature_for_specific_class, rowvar=False)
+     
 
-
-def likelihood_of_class(
-    feature: np.ndarray,
-    class_mean: np.ndarray,
-    class_covar: np.ndarray
-) -> float:
+def likelihood_of_class(feature: np.ndarray, class_mean: np.ndarray, class_covar: np.ndarray) -> float:
     '''
     Estimate the likelihood that a sample is drawn
     from a multivariate normal distribution, given the mean
     and covariance of the distribution.
     '''
     ...
+    return multivariate_normal(mean=class_mean, cov=class_covar).pdf(feature)
 
 
-def maximum_likelihood(
-    train_features: np.ndarray,
-    train_targets: np.ndarray,
-    test_features: np.ndarray,
-    classes: list
-) -> np.ndarray:
+
+def maximum_likelihood(train_features: np.ndarray, train_targets: np.ndarray, test_features: np.ndarray, classes: list) -> np.ndarray:
     '''
     Calculate the maximum likelihood for each test point in
     test_features by first estimating the mean and covariance
@@ -64,13 +57,21 @@ def maximum_likelihood(
     a [test_features.shape[0] x len(classes)] shaped numpy
     array
     '''
+
     means, covs = [], []
     for class_label in classes:
-        ...
-    likelihoods = []
+        means.append(mean_of_class(train_features, train_targets, class_label))
+        covs.append(covar_of_class(train_features, train_targets, class_label))        
+        
+    likelihoods = np.zeros((test_features.shape[0], len(classes)))
     for i in range(test_features.shape[0]):
-        ...
-    return np.array(likelihoods)
+        feature = test_features[i, :]
+        
+        for j in range(len(classes)):
+            likelihood = multivariate_normal.pdf(feature, mean=means[j], cov=covs[j])
+            likelihoods[i, j] = likelihood
+        
+    return likelihoods
 
 
 def predict(likelihoods: np.ndarray):
@@ -101,3 +102,21 @@ def maximum_aposteriori(
     array
     '''
     ...
+
+if __name__ == '__main__':
+    features, targets, classes = load_iris()
+    (train_features, train_targets), (test_features, test_targets) = split_train_test(features, targets, train_ratio=0.6)
+    
+    print("[+]Part 1.1")
+    print(mean_of_class(train_features, train_targets, 0))
+    
+    print("\n[+]Part 1.2")
+    print(covar_of_class(train_features, train_targets, 0))
+    
+    print("\n[+]Part 1.3")
+    class_mean = mean_of_class(train_features, train_targets, 0)
+    class_cov = covar_of_class(train_features, train_targets, 0)
+    print(likelihood_of_class(test_features[0, :], class_mean, class_cov))
+    
+    print("\n[+]Part 1.4")
+    print(maximum_likelihood(train_features, train_targets, test_features, classes))
