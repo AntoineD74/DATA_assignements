4a2838ccf1a5c3ea2bd5d31079857e836b6dec9f
antoine <antoine.dupuis74@icloud.com>
2023-10-11 13:13:32 +0000

    Part 1 done

--- 08_boosting/template.py
+++ 08_boosting/template.py
@@ -1,14 +1,3 @@
-# Author: 
-# Date:
-# Project: 
-# Acknowledgements: 
-#
-
-# NOTE: Your code should NOT contain any main functions or code that is executed
-# automatically.  We ONLY want the functions as stated in the README.md.
-# Make sure to comment out or remove all unnecessary code before submitting.
-
-
 import os
 import numpy as np
 import pandas as pd
@@ -28,8 +17,64 @@ def get_better_titanic():
     Loads the cleaned titanic dataset but change
     how we handle the age column.
     '''
-    pass
+    # Load in the raw data
+    # check if data directory exists for Mimir submissions
+    # DO NOT REMOVE
+    if os.path.exists('./data/train.csv'):
+        train = pd.read_csv('./data/train.csv')
+        test = pd.read_csv('./data/test.csv')
+    else:
+        train = pd.read_csv('train.csv')
+        test = pd.read_csv('test.csv')
+    
+    # Concatenate the train and test set into a single dataframe
+    # we drop the `Survived` column from the train set
+    X_full = pd.concat([train.drop('Survived', axis=1), test], axis=0)
 
+    # The cabin category consist of a letter and a number.
+    # We can divide the cabin category by extracting the first
+    # letter and use that to create a new category. So before we
+    # drop the `Cabin` column we extract these values
+    X_full['Cabin_mapped'] = X_full['Cabin'].astype(str).str[0]
+    # Then we transform the letters into numbers
+    cabin_dict = {k: i for i, k in enumerate(X_full.Cabin_mapped.unique())}
+    X_full.loc[:, 'Cabin_mapped'] =\
+        X_full.loc[:, 'Cabin_mapped'].map(cabin_dict)
+
+    # We drop multiple columns that contain a lot of NaN values
+    # in this assignment
+    # Maybe we should
+    
+    X_full["Age"].fillna(X_full["Age"].mean(), inplace=True)
+    X_full.drop(
+    ['Cabin', 'Name', 'Ticket'],
+    inplace=True, axis=1)
+    
+    # Instead of dropping the fare column we replace NaN values
+    # with the 3rd class passenger fare mean.
+    fare_mean = X_full[X_full.Pclass == 3].Fare.mean()
+    X_full['Fare'].fillna(fare_mean, inplace=True)
+    
+    # Instead of dropping the Embarked column we replace NaN values
+    # with `S` denoting Southampton, the most common embarking
+    # location
+    X_full['Embarked'].fillna('S', inplace=True)
+
+    # We then use the get_dummies function to transform text
+    # and non-numerical values into binary categories.
+    X_dummies = pd.get_dummies(
+        X_full,
+        columns=['Sex', 'Cabin_mapped', 'Embarked'],
+        drop_first=True)
+
+    # We now have the cleaned data we can use in the assignment
+    X = X_dummies[:len(train)]
+    submission_X = X_dummies[len(train):]
+    y = train.Survived
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.3, random_state=5, stratify=y)
+
+    return (X_train, y_train), (X_test, y_test), submission_X
 
 def rfc_train_test(X_train, t_train, X_test, t_test):
     '''
@@ -88,3 +133,10 @@ def _create_submission():
     pass
     prediction = None # !!! Your prediction here !!!
     build_kaggle_submission(prediction)
+
+
+if __name__ == '__main__':
+    print("[+]Part 1.1")
+    (tr_X, tr_y), (tst_X, tst_y), submission_X = get_better_titanic()
+    print(tr_X[:1])
+    
